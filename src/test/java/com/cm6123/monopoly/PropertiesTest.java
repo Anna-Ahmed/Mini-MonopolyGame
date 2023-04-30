8ae55e1bd4d0778dae434a447b66e6d69e0de028
Anna Ahmed <ahmeda66@cardiff.ac.uk>
2023-04-30 14:28:56 +0100

    Mehtod to buy old kent and pay rent created and testing for both players.

--- src/test/java/com/cm6123/monopoly/PropertiesTest.java
+++ src/test/java/com/cm6123/monopoly/PropertiesTest.java
@@ -79,6 +79,74 @@ public class PropertiesTest {
 
     }
 
+
+    @Test
+    public void player2BuyOldKent(){
+        MonopolyBoard board = new MonopolyBoard();
+        Properties oldkent = new Properties();
+        Player player = board.getPlayer2();
+
+
+
+
+
+
+        //Given players postion 3 (old kent) on the board
+        player.setPlayerPosition(3);
+
+        //when player buysoldkent
+        oldkent.buyOldKent(player);
+
+
+
+
+
+        //Then players balacne will be updated with the price of old kent reducted from balance;
+
+        int currentPlayerBalance = player.getPlayerBalance();
+        double expectedPlayerBalance = currentPlayerBalance ;
+
+
+
+        assertEquals(expectedPlayerBalance, player.getPlayerBalance());
+
+
+
+    }
+
+
+    @Test
+    public void player2PayRentOnOldKent(){
+        MonopolyBoard board = new MonopolyBoard();
+        Properties oldkent = new Properties();
+        Player player = board.getPlayer2();
+
+
+
+
+
+
+        //Given players postion is 3 (old kent on board)
+        player.setPlayerPosition(3);
+
+        //When player pays rent on old kent
+        oldkent.payOldKentRent(player);
+
+
+
+
+        //then player wills balance will update with rent price reducted from balance;
+        int currentPlayerBalance = player.getPlayerBalance();
+        double expectedPlayerBalance = currentPlayerBalance;
+
+
+
+        assertEquals(expectedPlayerBalance, player.getPlayerBalance());
+
+
+
+    }
+
 }
 
 
